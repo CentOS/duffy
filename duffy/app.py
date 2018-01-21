4d5aca1cb6009e04c459913ca874282c294ce62b
Brian Stinson <brian@bstinson.com>
2018-01-21 05:19:34 -0600

    allow for storing configuration in the /etc/duffy.conf file

--- duffy/app.py
+++ duffy/app.py
@@ -12,6 +12,7 @@ from duffy.config import ProdConfig,DevConfig
 def create_app(config_object=DevConfig):
     app = Flask(__name__.split('.')[0])
     app.config.from_object(config_object)
+    app.config.from_pyfile('/etc/duffy.conf')
     register_extensions(app)
     register_blueprints(app)
     register_errorhandlers(app)
