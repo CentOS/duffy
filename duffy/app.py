549b0b50a0a16cb8ab5ce1f037c50ad223ee6209
Brian Stinson <brian@bstinson.com>
2018-08-31 09:42:33 -0500

    explicitly look for /etc/duffy.conf

--- duffy/app.py
+++ duffy/app.py
@@ -6,13 +6,14 @@ from duffy import api_v1
 from duffy.types import seamicro
 
 from duffy.extensions import db, migrate, marshmallow
-from duffy.config import ProdConfig,DevConfig
+from duffy.config import ProdConfig, DevConfig
 
 
 def create_app(config_object=DevConfig):
     app = Flask(__name__.split('.')[0])
     app.config.from_object(config_object)
-    app.config.from_envvar('DUFFY_SETTINGS',silent=True)
+    app.config.from_envvar('DUFFY_SETTINGS', silent=True)
+    app.config.from_pyfile('/etc/duffy.conf', silent=True)
 
     register_extensions(app)
     register_blueprints(app)
