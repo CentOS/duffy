7c7b8455de4c91cd7728dcb49ddfc56e7564c37d
Brian Stinson <bstinson@centosproject.org>
2019-10-24 13:11:19 -0500

    bump release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='duffy',
     description='',
-    version='2.0.0',
+    version='2.0.1',
     packages=setuptools.find_packages(),
     include_package_data=True,
     license='Apache 2.0',
