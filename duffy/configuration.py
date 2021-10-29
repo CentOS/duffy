11c3f1b4af41717408a386ff70e675dc7a67f6b0
Nils Philippsen <nils@redhat.com>
2021-10-29 15:23:31 +0200

    Add configuration mechanism
    This will load configuration from /etc/duffy.yaml if present, or another
    configuration file specified on the command line. The configuration keys
    are the same as the click option variables.
    
    In the course, specify the name in click.command so the CLI won't be
    called differently in tests than for real, and relax tests processing
    help output so we don't have to update them any time we change options.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ duffy/configuration.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import List, Union
+
+import yaml
+
+SYSTEM_CONFIG_FILE = "/etc/duffy.yaml"
+
+config = {}
+
+DEFAULT_CONFIG = {
+    "loglevel": "warning",
+    "host": "127.0.0.1",
+    "port": 8080,
+}
+
+
+def read_configuration(*config_files: List[Union[Path, str]]):
+    config.clear()
+    config.update(DEFAULT_CONFIG)
+    for config_file in config_files:
+        if not isinstance(config_file, Path):
+            config_file = Path(config_file)
+        with config_file.open("r") as fp:
+            for config_doc in yaml.safe_load_all(fp):
+                config.update(config_doc)
