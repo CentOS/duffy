0dfaf1a942be542565547724d67d7e8cee3335d7
Nils Philippsen <nils@redhat.com>
2021-11-08 13:10:05 +0100

    Use --host/--port options
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/app/cli.py
+++ duffy/app/cli.py
@@ -5,21 +5,8 @@ from ..version import __version__
 
 
 @click.command()
-@click.option("-p", "--portnumb", "portnumb", help="Set the port value [0-65536]", default=8080)
-@click.option(
-    "-6",
-    "--ipv6",
-    "netprotc",
-    flag_value="ipv6",
-    help="Start the server on an IPv6 address",
-)
-@click.option(
-    "-4",
-    "--ipv4",
-    "netprotc",
-    flag_value="ipv4",
-    help="Start the server on an IPv4 address",
-)
+@click.option("-H", "--host", "host", help="Set the host address to listen on")
+@click.option("-p", "--port", "port", type=int, help="Set the port value [0-65536]")
 @click.option(
     "-l",
     "--loglevel",
@@ -29,20 +16,18 @@ from ..version import __version__
     default="info",
 )
 @click.version_option(version=__version__, prog_name="Duffy")
-def main(portnumb, netprotc, loglevel):
+def main(host, port, loglevel):
     """
     Duffy is the middle layer running ci.centos.org that manages the
     provisioning, maintenance and teardown / rebuild of the Nodes
     (physical hardware for now, VMs coming soon) that are used to run
     the tests in the CI Cluster.
     """
+    # Report for duty
     print(" * Starting Duffy...")
-    print(" * Port number : %s" % portnumb)
-    if netprotc == "ipv6":
-        print(" * IP version  : 6")
-        netpdata = "::"
-    else:
-        print(" * IP version  : 4")
-        netpdata = "0.0.0.0"
-    print(" * Log level   : %s" % loglevel)
-    uvicorn.run("duffy.app.main:app", host=netpdata, port=portnumb, log_level=loglevel)
+    print(f" * Host address : {host}")
+    print(f" * Port number  : {port}")
+    print(f" * Log level    : {loglevel}")
+
+    # Start the show
+    uvicorn.run("duffy.app.main:app", host=host, port=port, log_level=loglevel)
