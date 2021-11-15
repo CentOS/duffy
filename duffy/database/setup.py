9d35d901d13a34c6729baeb2809492e248776375
siddharthvipul <siddharthvipul1@gmail.com>
2021-11-15 11:44:50 +0530

    Add boilerplate for Alembic migrations
    This sets up structures in the database needed for Alembic to actually
    manage different versions of the database schema.
    
    Signed-off-by: siddharthvipul <siddharthvipul1@gmail.com>
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/database/setup.py
+++ duffy/database/setup.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+import alembic.command
+import alembic.config
+
+from ..configuration import config
+
 # Import the DB model here so its classes are considered by metadata.create_all() below.
 from ..database import model  # noqa: F401
 from . import get_sync_engine, metadata
@@ -12,3 +17,10 @@ def setup_db_schema():
     with engine.begin():
         print("Creating database schema")
         metadata.create_all(bind=engine)
+
+        print("Setting up database migrations")
+        cfg = alembic.config.Config()
+        cfg.set_main_option("script_location", str(HERE / "migrations"))
+        cfg.set_main_option("sqlalchemy.url", config["database"]["sqlalchemy"]["sync_url"])
+
+        alembic.command.stamp(cfg, "head")
--- tests/database/test_setup.py
+++ tests/database/test_setup.py
@@ -1,16 +1,35 @@
 from unittest import mock
 
+import pytest
+
 from duffy.database import setup
 
+TEST_CONFIG = {
+    "database": {
+        "sqlalchemy": {
+            "sync_url": "boo",
+        }
+    }
+}
 
+
+@pytest.mark.duffy_config(TEST_CONFIG)
+@mock.patch("duffy.database.setup.alembic.command.stamp")
+@mock.patch("duffy.database.setup.alembic.config.Config")
 @mock.patch("duffy.database.setup.metadata")
 @mock.patch("duffy.database.setup.get_sync_engine")
-def test_setup_db_schema(get_sync_engine, metadata):
+def test_setup_db_schema(get_sync_engine, metadata, Config, stamp):
     engine = mock.MagicMock()
     get_sync_engine.return_value = engine
+
+    cfg = mock.MagicMock()
+    Config.return_value = cfg
 
     setup.setup_db_schema()
 
     get_sync_engine.assert_called_once_with()
     engine.begin.assert_called_once_with()
     metadata.create_all.assert_called_once_with(bind=engine)
+    cfg.set_main_option.assert_any_call("script_location", str(setup.HERE / "migrations"))
+    cfg.set_main_option.assert_any_call("sqlalchemy.url", "boo")
+    stamp.assert_called_once_with(cfg, "head")
