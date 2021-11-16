994384c0ecf94ff53ff17be55003f6284dfeafcb
Nils Philippsen <nils@redhat.com>
2021-11-16 17:21:24 +0530

    Move cli module out of duffy.app
    We'll have subcommands soon, and most of them aren't about running the
    web app.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>
    Signed-off-by: siddharthvipul <siddharthvipul1@gmail.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,11 +4,11 @@ from unittest import mock
 import pytest
 from click.testing import CliRunner
 
-from duffy.app.cli import main
+from duffy.cli import main
 from duffy.version import __version__
 
 HERE = Path(__file__).parent
-EXAMPLE_CONFIG = HERE.parent.parent / "etc" / "duffy-example-config.yaml"
+EXAMPLE_CONFIG = HERE.parent / "etc" / "duffy-example-config.yaml"
 
 
 def test_cli_version():
@@ -36,7 +36,7 @@ def test_cli_suggestion():
 @pytest.mark.parametrize(
     "parameters", ((), ("--host=127.0.0.1",), (f"--config={EXAMPLE_CONFIG.absolute()}",))
 )
-@mock.patch("duffy.app.cli.uvicorn.run")
+@mock.patch("duffy.cli.uvicorn.run")
 def test_cli_main(uvicorn_run, parameters):
     runner = CliRunner()
     runner.invoke(main, parameters)
