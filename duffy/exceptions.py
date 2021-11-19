2c1f28cdb2a79412844d88e7a156876304686cfa
Nils Philippsen <nils@redhat.com>
2021-11-19 12:28:48 +0100

    Add custom exceptions for Duffy
    This is to enable better error messages (rather than exceptions being
    spat onto the console if e.g. some configuration key is missing).
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ duffy/exceptions.py
@@ -0,0 +1,6 @@
+class DuffyException(Exception):  # pragma: no cover
+    """Custom exceptions for Duffy."""
+
+
+class DuffyConfigurationError(DuffyException):  # pragma: no cover
+    """Something's wrong with the configuration of Duffy."""
