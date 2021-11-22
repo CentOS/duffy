ce26989dde6df0bcd8a1fd902d53f0c40b47c6ee
Nils Philippsen <nils@redhat.com>
2021-11-22 16:40:34 +0100

    Add an interactive shell
    The new `shell` subcommand initializes the connection to the database
    and runs an interactive shell – IPython if available, otherwise plain
    Python – with some useful objects in the local namespace.
    
    This adds `ipython` as an optional dependency as part of the new
    `interactive` set of extra dependencies.
    
    Additionally, its corresponding CLI test demonstrates how to override
    auto-detected values for CLI options of the type click.Choice – this is
    needed so that the unit tests run smoothly regardless of if ipython is
    available or not.
    
    Fixes: #96
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/exceptions.py
+++ duffy/exceptions.py
@@ -4,3 +4,7 @@ class DuffyException(Exception):  # pragma: no cover
 
 class DuffyConfigurationError(DuffyException):  # pragma: no cover
     """Something's wrong with the configuration of Duffy."""
+
+
+class DuffyShellUnavailableError(DuffyException):  # pragma: no cover
+    """The selected interactive shell type is not available."""
