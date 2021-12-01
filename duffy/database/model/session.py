ac5e665d54f19c62f07e1cb64ff7a01323c023a4
Nils Philippsen <nils@redhat.com>
2021-12-01 16:09:16 +0100

    Record creation, retirement of DB objects
    Derive the ORM classes from CreatableMixin, RetirableMixin and mark
    defaults to be eagerly loaded on INSERT.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/database/model/session.py
+++ duffy/database/model/session.py
@@ -2,11 +2,13 @@ from sqlalchemy import Column, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
 from .. import Base
+from ..util import CreatableMixin, RetirableMixin
 from .project import Project
 
 
-class Session(Base):
+class Session(Base, CreatableMixin, RetirableMixin):
     __tablename__ = "sessions"
+    __mapper_args__ = {"eager_defaults": True}
     id = Column(Integer, primary_key=True, nullable=False)
     project_id = Column(Integer, ForeignKey(Project.id), nullable=False)
     project = relationship(Project, backref="sessions")
