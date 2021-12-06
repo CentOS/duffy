c31b3c2a164403f602850e7183142a7df0b26c29
Nils Philippsen <nils@redhat.com>
2021-12-06 23:06:47 +0100

    Make setup-db fail if tables are present
    Fixes: #131
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/database/setup.py
+++ duffy/database/setup.py
@@ -1,7 +1,9 @@
+import sys
 from pathlib import Path
 
 import alembic.command
 import alembic.config
+from sqlalchemy import inspect
 
 from ..configuration import config
 
@@ -14,6 +16,16 @@ HERE = Path(__file__).parent
 
 def setup_db_schema():
     engine = get_sync_engine()
+
+    inspection_result = inspect(engine)
+
+    present_tables = sorted(n for n in metadata.tables if inspection_result.has_table(n))
+
+    if present_tables:
+        print(f"Tables already present: {', '.join(present_tables)}", file=sys.stderr)
+        print("Refusing to change database schema.", file=sys.stderr)
+        sys.exit(1)
+
     with engine.begin():
         print("Creating database schema")
         metadata.create_all(bind=engine)
