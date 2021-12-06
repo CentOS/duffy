be9652cb91922a8ac5451a81a57be54eae6dee47
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2021-12-06 12:41:05 +0000

    Extend automatically generated docs
    Co-authored-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>
    Co-authored-by: Ben Capper <BenCapper19@gmail.com>
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- duffy/app/main.py
+++ duffy/app/main.py
@@ -2,10 +2,30 @@ import logging
 
 from fastapi import FastAPI
 
+from ..version import __version__
 from .controllers import chassis, node, session, tenant
 
 log = logging.getLogger(__name__)
-app = FastAPI()
+
+description = """
+Duffy is the middle layer running [`ci.centos.org`](https://ci.centos.org). It provisions, tears
+down and rebuilds physical and virtual machines are used to run tests in the CentOS CI Cluster.
+"""
+
+tags_metadata = [
+    {"name": "nodes", "description": "Operations with physical and virtual nodes"},
+    {"name": "sessions", "description": "Operations with sessions"},
+    {"name": "tenants", "description": "Operations with tenants"},
+    {"name": "chassis", "description": "Operations with chassis"},
+]
+
+app = FastAPI(
+    title="Duffy",
+    description=description,
+    version=__version__,
+    contact={"name": "CentOS CI", "email": "ci-sysadmin@centos.org"},
+    openapi_tags=tags_metadata,
+)
 
 # API v1
 
