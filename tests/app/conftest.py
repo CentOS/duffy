45212947ab950a16a3164c19e16e66e234a819f8
Nils Philippsen <nils@redhat.com>
2021-12-14 11:16:56 +0100

    Add doc strings to fixtures
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/app/conftest.py
+++ tests/app/conftest.py
@@ -6,5 +6,6 @@ from duffy.app.main import app
 
 @pytest.fixture
 async def client() -> AsyncClient:
+    """A fixture creating an async client for testing the FastAPI app."""
     async with AsyncClient(app=app, base_url="http://duffy-test.example.com") as client:
         yield client
