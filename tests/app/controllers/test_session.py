260581bf1a1b779a876078ff93185a072754b741
Nils Philippsen <nils@redhat.com>
2021-12-16 21:09:06 +0100

    Improve test to create session with unknown tenant
    Check the error detail contents and explain why the tenant won't get
    created.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/app/controllers/test_session.py
+++ tests/app/controllers/test_session.py
@@ -1,3 +1,5 @@
+import re
+
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from . import BaseTestController
@@ -13,7 +15,8 @@ class TestSession(BaseTestController):
     }
 
     async def test_create_unknown_tenant(self, client):
+        # setting tenant_id manually will skip the code which would create the tenant
         response = await self._create_obj(client, attrs={"tenant_id": 1})
         assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
         result = response.json()
-        assert "detail" in result
+        assert re.match(r"^can't find tenant with id \d+$", result["detail"])
