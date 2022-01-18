1946554c0741b6e5a55941aa6514e76327428f24
Nils Philippsen <nils@redhat.com>
2022-01-18 13:53:08 +0100

    Add a function to do deep merges of dicts
    This is needed to properly merge configuration, either from multiple
    configuration files or later in the configuration of node pools, to let
    users inherit the configuration of one node pool from that of another
    (abstract or concrete one).
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/util.py
+++ duffy/util.py
@@ -22,3 +22,31 @@ def camel_case_to_lower_with_underscores(camelcased: str) -> str:
     return "_".join(
         lowercased[prev_split_pos:split_pos] for prev_split_pos, split_pos in chunk_positions
     )
+
+
+def merge_dicts(*src_dicts):
+    """Create a deep merge of several dictionaries.
+
+    The structure of the dictionaries must be compatible, i.e. sub-dicts
+    may not be differently typed between the source dictionaries."""
+    if not src_dicts:
+        raise ValueError("Can't merge nothing")
+
+    if not all(isinstance(src_dict, dict) for src_dict in src_dicts):
+        raise TypeError("All objects to be merged have to be dictionaries")
+
+    res_dict = {}
+
+    for src_dict in src_dicts:
+        for key, src_value in src_dict.items():
+            if isinstance(src_value, dict):
+                if key not in res_dict:
+                    res_dict[key] = src_value.copy()
+                else:
+                    res_dict[key] = merge_dicts(res_dict[key], src_value)
+            elif key in res_dict and isinstance(res_dict[key], dict):
+                raise TypeError("All objects to be merged have to be dictionaries")
+            else:
+                res_dict[key] = src_value
+
+    return res_dict
