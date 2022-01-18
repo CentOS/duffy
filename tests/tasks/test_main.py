a5b1d1653770fd553195cd1a30110550d22ce129
Nils Philippsen <nils@redhat.com>
2022-01-18 16:28:54 +0100

    Add NodePool classes
    The NodePool classes are beefed-up dictionaries which process node pool
    configuration managing inheritance between abstract and concrete pools
    and offer functionality to provision and deprovision nodes.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/tasks/test_main.py
+++ tests/tasks/test_main.py
@@ -4,14 +4,16 @@ from duffy.tasks import main
 
 
 @mock.patch("duffy.tasks.main.celery")
+@mock.patch("duffy.tasks.main.NodePool")
 @mock.patch("duffy.tasks.main.init_tasks")
 @mock.patch("duffy.tasks.main.database")
-def test_start_worker(database, init_tasks, celery):
+def test_start_worker(database, init_tasks, NodePool, celery):
     """Test that start_worker() passes on arguments to Celery."""
     worker_args = ("foo", "--bar")
     main.start_worker(worker_args=worker_args)
 
     database.init_sync_model.assert_called_once_with()
-
     init_tasks.assert_called_once_with()
+    NodePool.process_configuration.assert_called_once_with()
+
     celery.worker_main.assert_called_once_with(("worker",) + worker_args)
