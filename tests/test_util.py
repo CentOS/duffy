9a6cba200b39b167109f90c978eaa9afee24dd1d
Nils Philippsen <nils@redhat.com>
2022-01-18 13:53:08 +0100

    Add a function to do deep merges of dicts
    This is needed to properly merge configuration, either from multiple
    configuration files or later in the configuration of node pools, to let
    users inherit the configuration of one node pool from that of another
    (abstract or concrete one).
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,10 @@
+from typing import List, Tuple, Union
+
 import pytest
 
-from duffy.util import camel_case_to_lower_with_underscores
+from duffy.util import camel_case_to_lower_with_underscores, merge_dicts
+
+from .util import noop_context
 
 
 @pytest.mark.parametrize(
@@ -13,3 +17,29 @@ from duffy.util import camel_case_to_lower_with_underscores
 )
 def test_camel_case_to_lower_with_underscores(camelcased, converted):
     assert camel_case_to_lower_with_underscores(camelcased) == converted
+
+
+class TestMergeDicts:
+    """Test the merge_dicts() function."""
+
+    # this contains tuples of ([input_dict1, input_dict2, ...], expected result or exception)
+    test_cases: List[Tuple[List[dict], Union[dict, Exception]]] = [
+        ([{"a": 1}, {"b": 2}], {"a": 1, "b": 2}),
+        ([{"a": {"b": 1}}, {"a": {"b": 2, "c": 3}, "d": 4}], {"a": {"b": 2, "c": 3}, "d": 4}),
+        ([], ValueError),
+        ([{"a": {"b": 1}}, {"a": 5}], TypeError),
+        ([{"a": 5}, {"a": {"b": 1}}], TypeError),
+    ]
+
+    @pytest.mark.parametrize("input_dicts,expected", test_cases)
+    def test_merge_dicts(self, input_dicts, expected):
+        if isinstance(expected, type) and issubclass(expected, Exception):
+            expectation = pytest.raises(expected)
+        else:
+            expectation = noop_context()
+
+        with expectation:
+            result = merge_dicts(*input_dicts)
+
+        if isinstance(expected, dict):
+            assert expected == result
