a1c7402c5137bfe280383bf541d1af8e5f243e03
Nils Philippsen <nils@redhat.com>
2022-01-31 11:12:13 +0100

    Initialize DB connection in task workers
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/tasks/main.py
+++ duffy/tasks/main.py
@@ -1,9 +1,12 @@
 from typing import Tuple
 
+from .. import database
 from ..configuration import config
 from .base import celery
 
 
 def start_worker(worker_args: Tuple[str]):
+    database.init_sync_model()
+
     celery.config_from_object(config["celery"])
     celery.worker_main(("worker",) + worker_args)
