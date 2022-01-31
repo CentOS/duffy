62e9d0d0922b53bc8b2c5dd62372cefbfbf44ea2
Nils Philippsen <nils@redhat.com>
2022-01-31 11:12:13 +0100

    Initialize DB connection in task workers
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/tasks/test_main.py
+++ tests/tasks/test_main.py
@@ -12,10 +12,13 @@ CELERY_CONFIG = {
 
 @pytest.mark.duffy_config({"celery": CELERY_CONFIG})
 @mock.patch("duffy.tasks.main.celery")
-def test_start_worker(celery):
+@mock.patch("duffy.tasks.main.database")
+def test_start_worker(database, celery):
     """Test that start_worker() passes on arguments to Celery."""
     worker_args = ("foo", "--bar")
     main.start_worker(worker_args=worker_args)
+
+    database.init_sync_model.assert_called_once_with()
 
     celery.config_from_object.assert_called_once_with(CELERY_CONFIG)
     celery.worker_main.assert_called_once_with(("worker",) + worker_args)
