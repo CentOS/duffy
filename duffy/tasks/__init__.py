e7966b099ec5e1e50230da2b0708e9393c8d1082
Nils Philippsen <nils@redhat.com>
2022-02-01 12:43:18 +0100

    Add tasks to fill node pools
    The fill_pools() and fill_single_pool() tasks check if node pools are
    filled to their configured levels and provision new nodes otherwise.
    They can be called from endpoint controllers (if nodes have been taken
    out of a pool) but also at periodic intervals and at startup of task
    workers to ensure pools have their configured count of nodes available
    to hand out.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- duffy/tasks/__init__.py
+++ duffy/tasks/__init__.py
@@ -1,2 +1,3 @@
 from .base import celery, init_tasks  # noqa: F401
 from .main import start_worker  # noqa: F401
+from .provision import fill_pools, fill_single_pool  # noqa: F401
--- tests/tasks/test_main.py
+++ tests/tasks/test_main.py
@@ -1,6 +1,42 @@
 from unittest import mock
 
+import pytest
+
 from duffy.tasks import main
+
+TEST_CONFIG = {
+    "tasks": {
+        "celery": {
+            "broker_url": "redis://localhost:6379",
+            "result_backend": "redis://localhost:6379",
+        },
+        "periodic": {"fill-pools": {"interval": 5}},
+    }
+}
+
+
+@pytest.mark.duffy_config(TEST_CONFIG)
+@mock.patch("duffy.tasks.main.fill_pools")
+def test_setup_periodic_tasks(fill_pools):
+    sender = mock.MagicMock()
+    fill_pools.signature.return_value = sentinel = object()
+
+    main.setup_periodic_tasks(sender)
+
+    fill_pools.signature.assert_called_once_with()
+    sender.add_periodic_task.assert_called_once_with(
+        TEST_CONFIG["tasks"]["periodic"]["fill-pools"]["interval"], sentinel
+    )
+
+
+@mock.patch("duffy.tasks.main.fill_pools")
+def test_run_init_tasks(fill_pools):
+    fill_pools.delay.return_value = delayed_task = mock.Mock()
+
+    main.run_init_tasks(None)
+
+    fill_pools.delay.assert_called_once_with()
+    delayed_task.forget.assert_called_once_with()
 
 
 @mock.patch("duffy.tasks.main.celery")
