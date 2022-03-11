c698a639907562c29735b275c51d10ae50798400
Nils Philippsen <nils@redhat.com>
2022-03-11 12:01:21 +0100

    Add custom Pydantic field types for timedeltas
    In Pydantic models, plain timedelta fields aren't very user-friendly,
    you have to use either of these two formats:
    
    - [-][DD ][HH:MM]SS[.ffffff]
    - [±]P[DD]DT[HH]H[MM]M[SS]S
    
    Additionally, the first option isn't clearly distinguishable from
    datetime fields which accept plain integer or float strings and
    interpret them as Unix time.
    
    This adds two field types, APITimeDelta and ConfigTimeDelta. Both accept
    strings of a sign (mandatory "+" or "-" for APITimeDelta, optional "+"
    for ConfigTimeDelta which doesn't allow negative values) followed by
    fields for weeks, days, hours, minutes, seconds and milliseconds of a
    numerical value and an abbreviated suffix.  Each field is optional but
    at least one must be present.
    
    Valid examples:
    
    "1h" -> 1 hour duration (for ConfigTimeDelta)
    "+3h30m" -> 3 hours and 30 minutes in the future
    "-20s" -> 20 seconds in the past (for APITimeDelta)
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ tests/test_misc.py
@@ -0,0 +1,70 @@
+from datetime import timedelta
+
+import pytest
+from pydantic import BaseModel
+
+from duffy.misc import APITimeDelta, ConfigTimeDelta, _TimeDelta
+
+
+class _BaseTestTimeDelta:
+    cls_to_test: _TimeDelta
+
+    def test_validate(self, test_input):
+        expected = self.input_to_expected[test_input]
+
+        if isinstance(expected, type) and issubclass(expected, Exception):
+            with pytest.raises(expected):
+                self.cls_to_test.validate(test_input)
+        else:
+            validated = self.cls_to_test.validate(test_input)
+            assert validated == expected
+
+    def test_schema(self):
+        class FooModel(BaseModel):
+            td: self.cls_to_test
+
+        schema = FooModel.schema()
+
+        td = schema["properties"]["td"]
+        pattern = td["pattern"]
+        assert pattern.startswith("^")
+        assert pattern.endswith("$")
+        examples = td["examples"]
+        assert isinstance(examples, list)
+        assert all(isinstance(item, str) for item in examples)
+
+
+class TestAPITimeDelta(_BaseTestTimeDelta):
+
+    cls_to_test = APITimeDelta
+
+    input_to_expected = {
+        "+3h30m": timedelta(hours=3, minutes=30),
+        "-2d": timedelta(days=-2),
+        "1h": ValueError,
+        "": ValueError,
+        "+": ValueError,
+        object(): TypeError,
+    }
+
+    @pytest.mark.parametrize("test_input", input_to_expected)
+    def test_validate(self, test_input):
+        super().test_validate(test_input)
+
+
+class TestConfigTimeDelta(_BaseTestTimeDelta):
+
+    cls_to_test = ConfigTimeDelta
+
+    input_to_expected = {
+        "+3h30m": timedelta(hours=3, minutes=30),
+        "1h": timedelta(hours=1),
+        "-2d": ValueError,
+        "": ValueError,
+        "+": ValueError,
+        object(): TypeError,
+    }
+
+    @pytest.mark.parametrize("test_input", input_to_expected)
+    def test_validate(self, test_input):
+        super().test_validate(test_input)
