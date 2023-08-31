4575e772e11c582f6e3ec6c940e8f0453fdc7816
Nils Philippsen <nils@redhat.com>
2023-08-31 19:26:07 +0200

    Add short id and optional strings to log records
    This should really have been in #914.
    
    Related: #915
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/app/test_logging.py
+++ tests/app/test_logging.py
@@ -1,10 +1,41 @@
+import logging
 import uuid
 from unittest import mock
 
 import pytest
 
-from duffy.app import logging
+from duffy.app import logging as app_logging
 from duffy.app.middleware import request_id_ctxvar
+
+
+def test_lazy_short_request_id():
+    long_id = uuid.uuid4()
+
+    short_id = app_logging.LazyShortRequestId(long_id)
+
+    with mock.patch.object(app_logging, "str", wraps=str) as wrapped_str:
+        assert str(short_id) == short_id.data == str(long_id)[-12:]
+        wrapped_str.assert_called_once_with(long_id)
+
+        wrapped_str.reset_mock()
+
+        assert str(short_id) == short_id.data == str(long_id)[-12:]
+        wrapped_str.assert_not_called()
+
+
+def test_lazy_formatted_string():
+    foo = mock.Mock()
+    foo.__str__ = wrapped_str = mock.Mock(return_value="foo")
+
+    lazy_str = app_logging.LazyFormattedString("{foo}", foo=foo)
+
+    assert str(lazy_str) == lazy_str.data == "foo"
+    wrapped_str.assert_called_once_with()
+
+    wrapped_str.reset_mock()
+
+    assert str(lazy_str) == lazy_str.data == "foo"
+    wrapped_str.assert_not_called()
 
 
 class TestRequestIdFilter:
@@ -18,10 +49,18 @@ class TestRequestIdFilter:
 
         record = mock.Mock()
 
-        retval = logging.RequestIdFilter().filter(record)
+        retval = app_logging.RequestIdFilter().filter(record)
 
         assert retval is True
         assert record.request_id is expected
 
         if id_set:
+            expected_short = str(expected)[-12:]
+            assert record.short_request_id == expected_short
+            assert record.request_id_optional == f"[{expected}] "
+            assert record.short_request_id_optional == f"[{expected_short}] "
             request_id_ctxvar.reset(token)
+        else:
+            assert record.short_request_id is None
+            assert record.request_id_optional == ""
+            assert record.short_request_id_optional == ""
